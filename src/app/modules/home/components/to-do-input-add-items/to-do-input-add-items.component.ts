a84e66a13d0716a8b4a4bfe7ac4a43a7cdcbadb2
gustas01 <gustasboyevolution@hotmail.com>
2022-11-10 15:05:32 -0300

    criando componentes da estrutura do projeto

---
+++ src/app/modules/home/components/to-do-input-add-items/to-do-input-add-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-do-input-add-items',
+  templateUrl: './to-do-input-add-items.component.html',
+  styleUrls: ['./to-do-input-add-items.component.scss']
+})
+export class ToDoInputAddItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
